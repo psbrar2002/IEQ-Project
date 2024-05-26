065643a3c146ce25180d5cb7309f7ee6b1b965fc
Prabhdeep Brar <psbrar2002@gmail.com>
2024-05-25 19:37:02 -0700

    Added a Spinner for home type and made it a drop down menu.

--- app/src/main/java/com/example/ieqproject/MainActivity.kt
+++ app/src/main/java/com/example/ieqproject/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.ieqproject
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ieqproject.DwellingAttributes
+import com.example.ieqproject.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize the spinner
+        val homeTypeSpinner: Spinner = findViewById(R.id.homeTypeSpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.home_type_options,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            homeTypeSpinner.adapter = adapter
+        }
     }
 }
