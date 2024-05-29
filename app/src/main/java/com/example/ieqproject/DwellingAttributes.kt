62cd9b0afeaca5caad93024d0519d2255eeb6bce
Prabhdeep Brar <psbrar2002@gmail.com>
2024-05-29 12:09:21 -0700

    DwellingAttributes added

---
+++ app/src/main/java/com/example/ieqproject/DwellingAttributes.kt
@@ -0,0 +1,9 @@
+package com.example.ieqproject
+
+data class DwellingAttributes(
+    val homeType: String,
+    val isSection8: Boolean,
+    val isOaklandHousing: Boolean,
+    val numPeople: Int,
+    // Add other fields as needed
+)
