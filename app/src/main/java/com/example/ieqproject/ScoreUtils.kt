111a04c6750cd8ef2784a2eb1057ebc687b0395c
Prabhdeep Brar <psbrar2002@gmail.com>
2024-07-30 13:21:59 -0700

    File to calculate IEQ score

---
+++ app/src/main/java/com/example/ieqproject/ScoreUtils.kt
@@ -0,0 +1,26 @@
+package com.example.ieqproject.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.widget.TextView
+
+object ScoreUtils {
+
+    fun updateIEQScore(context: Context, sharedPreferences: SharedPreferences, ieqScoreTextView: TextView) {
+        // Retrieve individual scores from SharedPreferences
+        val iaqScore = sharedPreferences.getFloat("iaqScore", 0f).toDouble()
+        val acousticComfortScore = sharedPreferences.getFloat("acousticComfortScore", 0f).toDouble()
+        val hvacScore = sharedPreferences.getFloat("hvacScore", 0f).toDouble()
+        val thermalComfortScore = sharedPreferences.getFloat("thermalComfortScore", 0f).toDouble()
+
+        // Calculate the total score including the thermal comfort score
+        val totalScore = iaqScore + acousticComfortScore + hvacScore + thermalComfortScore
+
+        // Assuming the total possible score for these categories is 40
+        val ieqScore = (totalScore / 40) * 100
+
+        // Display the IEQ Score
+        ieqScoreTextView.text = "IEQ Score: %.2f%%".format(ieqScore)
+        sharedPreferences.edit().putFloat("ieqScore", ieqScore.toFloat()).apply()
+    }
+}
